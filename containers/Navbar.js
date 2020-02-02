55410bc55680fb36f292fc074cb6927c0e86c1ff
Lukasz Walczak <lukeabove@gmail.com>
2020-02-02 12:42:34 +0100

    data caching added

--- containers/Navbar.js
+++ containers/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
     this.state = {
       languages: ["All", "JS", "Ruby", "Java", "Go", "Python"],
       currentLanguage: "All",
-      repos: null
+      repos: {}
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
@@ -18,14 +18,27 @@ class Navbar extends Component {
   componentDidMount() {
     // fetch repos for all languages
     fetchRepos(this.state.currentLanguage).then(repos => {
-      this.setState({ repos });
+      this.setState(state => {
+        const updatedRepos = { ...state.repos };
+        updatedRepos[this.state.currentLanguage] = repos;
+        return { repos: updatedRepos };
+      });
     });
   }
 
   updateLanguage(currentLanguage) {
-    fetchRepos(currentLanguage).then(repos => {
-      this.setState({ repos, currentLanguage });
-    });
+    if (!this.state.repos[currentLanguage]) {
+      fetchRepos(currentLanguage).then(repos => {
+        this.setState(state => {
+          const updatedRepos = { ...state.repos }; // spread and create new object, not a reference to existing one
+          updatedRepos[currentLanguage] = repos;
+          return { repos: updatedRepos, currentLanguage };
+        });
+      });
+    } else {
+      this.setState({ currentLanguage });
+    }
+
     // this.state.currentLanguage = "state changed";
     // it will mutate the state, but it will not cause rerender
   }
